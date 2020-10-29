fb57566d5a51b5215107583b9e3c41e35600f2b5
Shoumik <shoumikrp2000@gmail.com>
2020-10-29 23:00:07 +0530

    Ui Changes

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:helpnetic/pages/emoji_screen.dart';
 import 'package:helpnetic/pages/image_screen.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_circular_text/circular_text.dart';
 
 Future<int> _getLanguage() async {
   final _prefs = await SharedPreferences.getInstance();
@@ -34,12 +35,99 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     List<String> languages = ['English', 'हिंदी'];
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Row(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () async {
+                  final _prefs = await SharedPreferences.getInstance();
+                  _prefs.setInt('index', language);
+                  _prefs.setString('language', languages[language]);
+                  Navigator.pushNamed(context, ImageScreen.id);
+                },
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: width / 6,
+                      backgroundImage: AssetImage('assets/images/Apple.jpg'),
+                    ),
+                    CircularText(
+                      radius: width / 4,
+                      children: [
+                        TextItem(
+                          text: Text(
+                            "Picture Mode".toUpperCase(),
+                            style: TextStyle(
+                              fontSize: 28,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          space: 12,
+                          startAngle: -90,
+                          startAngleAlignment: StartAngleAlignment.center,
+                          direction: CircularTextDirection.clockwise,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: width / 3,
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: width / 3,
+              ),
+              FlatButton(
+                onPressed: () async {
+                  final _prefs = await SharedPreferences.getInstance();
+                  _prefs.setInt('index', language);
+                  _prefs.setString('language', languages[language]);
+                  Navigator.pushNamed(context, EmojiScreen.id);
+                },
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: width / 6,
+                      backgroundImage: AssetImage('assets/images/Smile.png'),
+                    ),
+                    CircularText(
+                      radius: width / 4,
+                      children: [
+                        TextItem(
+                          text: Text(
+                            "Emoji Mode".toUpperCase(),
+                            style: TextStyle(
+                              fontSize: 28,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          space: 12,
+                          startAngle: -90,
+                          startAngleAlignment: StartAngleAlignment.center,
+                          direction: CircularTextDirection.clockwise,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
           RoundedButton(
             title: "Picture Mode",
             onPressed: () async {
